c8fa02ffe9855e325b5a308d12066d82f6e16db9
TiSHA <tisha_sutherland2001@aol.com>
2016-05-06 13:56:12 -0400

    whatever helpful message you want to have here

--- src/main/java/tiy/webapp/TiyBankingSystemApplication.java
+++ src/main/java/tiy/webapp/TiyBankingSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TiyBankingSystemApplication {
 
+	//my comment
 	public static void main(String[] args) {
 		SpringApplication.run(TiyBankingSystemApplication.class, args);
 	}
